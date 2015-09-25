ccfc3e6aa34bbff65523254bba8d28fa95ca1aa2
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-24 19:31:16 -1000

    creat function for mergeSort. Add descriptions of mergeSort.  add comments for function

---
+++ src/mergeSort.js
@@ -0,0 +1,53 @@
+// Merge Sort
+
+// Continuously divides the array length in half until two numbers
+// remain to compare which is lower.  The lower number is pushed into
+// an array.  The remaining numbers continue comparing, the smaller
+// number is pushed into the array until the last (largest) number remains.
+
+
+var array = [3,7,1,9,3,2,6,4];
+
+function mergeSort(array) {
+  // if array only has integer 0 and 1 array is sorted
+  if(array.length < 2)
+    return array;
+
+  // sort an array with numbers higher than first condition
+  // find the middle of the array
+  var middle = parseInt(array.length)/2;
+  // create array of the first half of the original array
+  var firstHalf = array.splice(0, middle);
+  // create array of the second half of the original array
+  var secondHalf = array.splice(middle, array.length);
+
+  // continues merging the newly created array until only one number remains when arrays are divided
+  return merge(mergeSort(left), mergeSort(right));
+
+
+}
+
+function merge(left, right)
+{
+    var result = [];
+  // compare the left side and the right side of each remerged array
+    while (left.length && right.length) {
+        // if the left number is smaller push into the result array
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+          // push right number into result array if smaller
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
+}
+
+console.log(mergeSort(a));
