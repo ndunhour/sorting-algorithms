5ce9e095b1b32586a3413936a0971be2b861fe89
ndunhourowens <ndunhour@hotmail.com>
2015-09-28 12:01:46 -1000

    create a css file for the graphics

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -7,30 +7,68 @@
 // order from least to greatest.
 
 
-var array = [3,7,1];
-function bubbleSort(array) {
+var unsortedArray = [3,7,1, 5,8,15,11];
+function bubbleSort(unsortedArray) {
   // create a do while loop.
   // purpose of do-while is to test if swap happened in the for loop
 
   var swap = false;
   do {
     // for loop iterates through the array
-    for(var i = 0; i < array.length; i++) {
+    for(var i = 0; i < unsortedArray.length; i++) {
       // temp storage for data during the swap
       var temp;
       // comparing the current index with next to find the least on of the two
-      if(array[i]>array[i+1]) {
-        // assigning position array[i] in the temp
-        temp = array[i];
-        // assigning position array[i+1] in position array[i]
-        array[i+1] = array[i];
-        // assigning position array[i+1] to data in temp
-        array[i+1] = temp;
+      if(unsortedArray[i]>unsortedArray[i+1]) {
+        // assigning position unsortedArray[i] in the temp
+        temp = unsortedArray[i];
+        // assigning position unsortedArray[i+1] in position unsortedArray[i]
+        unsortedArray[i+1] = unsortedArray[i];
+        // assigning position unsortedArray[i+1] to data in temp
+        unsortedArray[i+1] = temp;
         // indicate a swap occured
         swap = true;
       }
     }
+        console.log('work you piece of shit');
     // condition that indicats a swap as false and to exit the do-while
   } while (swap);
-  return array;
+  console.log(unsortedArray);
+  // return unsortedArray;
 }
+bubbleSort(unsortedArray);
+
+
+// //============= div id="bubbleGraph"===============//
+// function createGraph(unsortedArray) {
+
+//   var counter = 0;
+
+//   for(var i = 0; i < unsortedArray.length; i++) {
+
+//     var p = document.createElement('div');
+//     p.style.background = 'black';
+//     p.style.width = unsortedArray[i] + 50 + 'px';
+//     p.style.height = '30px';
+//     p.setAttribute('id', counter++);
+//     var element = document.getElementById('bubbleGraph');
+//     element.appendChild(p);
+//   }
+// }
+
+
+// window.onload = function(){
+//   createGraph(unsortedArray);
+//   // bubbleSort(unsortedArray);
+// };
+// //============= div id="runIt"===============//
+
+// var runIt_button = document.createElement('button');
+// runIt_button.appendChild(document.createTextNode('RUN IT'));
+// runIt_button.addEventListener('click', function() {
+//   bubbleSort(unsortedArray);
+//   });
+
+
+// var button = document.getElementById('button');
+// button.appendChild(runIt_button);
